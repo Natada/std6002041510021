179bee6894f24640a7eeafb92ff5bc10ecd35a2d
ณัฐดา ยาสมุด <s6002041510021@Macmini-213-17.local>
2019-03-14 17:00:43 +0700

    Create App component

--- component/App.js
+++ component/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
         return (
             //component about react native
             <View>
-                <Text>Hello World!!</Text>
+                <Text>Hello Natada!!</Text>
             </View>
 
         );
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-//import App from './App';
+//import App from './App';1
 import {name as appName} from './app.json';
 import App from './component/App';
 
