ae9e14b98ddb8f28ce0c1bfce0d18c7eb98602b2
ณัฐดา ยาสมุด <s6002041510021@Macmini-213-17.local>
2019-03-28 15:11:25 +0700

    cardsection in app

---
+++ components/CardSection.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class CardSection extends Component {
+    render() {
+        return (
+            <View>
+                <Text>XXXX</Text>
+            </View>
+        );
+    }
+}
+
+export default CardSection;
