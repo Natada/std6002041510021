ccf72e6a0b48a00d079d1161d5eb26d15d74e5ef
ณัฐดา ยาสมุด <s6002041510021@Macmini-213-17.local>
2019-03-28 15:21:44 +0700

    android app

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-//import App from './App';1
+import App from './components/App';
 import {name as appName} from './app.json';
-import App from './component/App';
 
 AppRegistry.registerComponent(appName, () => App);
